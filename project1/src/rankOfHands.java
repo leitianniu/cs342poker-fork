cd7454c77e365b5fd4e75b8abe7ea58133c691f3
Tianniu <Tianniu@Tianniu-ASUS.hsd1.il.comcast.net>
2014-02-08 21:10:22 -0600

    added Adrian's hand_matching

--- project1/src/rankOfHands.java
+++ project1/src/rankOfHands.java
@@ -3,8 +3,15 @@ import java.util.*;
 
 public class rankOfHands{
 	
-	public static void hand_calculator(List<Card> player_hand){
-		
-		
+	public static void hand_pairing(List<Card> player_hand){
+		for(int i = 0; i < 5; i++){
+			for(int j = i+1; j < 5; j++){
+				if(player_hand.get(i).getValue() == player_hand.get(j).getValue()){
+					player_hand.get(i).incr_matching();
+					player_hand.get(j).incr_matching();
+				}
+			}
+		}
+
 	}
 }
